b0a4f863a09afedc48b1dd13eff15ce35fdf2aea
Artyom Bakhtin <a@bakhtin.net>
2018-03-27 19:58:06 +0300

    remove Dockerfile symlinks
    Signed-off-by: Artyom Bakhtin <a@bakhtin.net>

--- docker/release/aarch64/entrypoint.sh
+++ docker/release/aarch64/entrypoint.sh
@@ -1,1 +1,5 @@
-../x86_64/entrypoint.sh
+#!/usr/bin/env bash
+echo key=$KEY
+echo $PWD
+iroha-cli --genesis_block --peers_address peers.list
+irohad --genesis_block genesis.block --config config.sample --keypair_name $KEY
